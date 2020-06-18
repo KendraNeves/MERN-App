32bb428beb3c543cd4125c91c5ad445d6a184684
Kendra Neves <kendraneves@gmail.com>
2020-06-18 12:40:45 -0400

    Server updated, models folder with user and book schemas added

---
+++ backend/models/book.model.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const bookSchema = new Schema({
+  username: { type: String, required: true },
+  title: { type: String, required: true },
+  author: { type: String, required: true },
+  description: { type: String },
+  image: { type: String },
+  link: { type: String },
+}, {
+  timestamps: true,
+});
+
+const Book = mongoose.model('Book', bookSchema);
+
+module.exports = Book;
