b2fb8669eaf0ead6b49f0d0a3c328f8f46a23333
Kendra Neves <kendraneves@gmail.com>
2020-06-18 15:39:59 -0400

    End points tested, backend functioning

--- backend/routes/books.js
+++ backend/routes/books.js
@@ -31,4 +31,31 @@ router.route('/add').post((req, res) => {
     .catch(err => res.status(400).json('Error: ' + err));
 });
 
+router.route('/:id').get((req, res) => {
+  Book.findById(req.params.id)
+    .then(book => res.json(book))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/:id').delete((req, res) => {
+  Book.findByIdAndDelete(req.params.id)
+    .then(() => res.json('Book deleted.'))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/update/:id').post((req, res) => {
+  Book.findById(req.params.id)
+    .then(book => {
+      book.username = req.body.username;
+      book.description = req.body.description;
+      book.duration = Number(req.body.duration);
+      book.date = Date.parse(req.body.date);
+
+      book.save()
+        .then(() => res.json('Book updated!'))
+        .catch(err => res.status(400).json('Error: ' + err));
+    })
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
 module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const bodyParser = require("body-parser")
+
 
 require('dotenv').config();
 
@@ -8,7 +10,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
-app.use(express.json());
+app.use(bodyParser.json());
 
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true }
